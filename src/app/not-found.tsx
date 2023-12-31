6d8b45bf010c9efef760df4d7cc3d3ac45ff345d
jd-galvan <jdgsuazo@gmail.com>
2023-12-31 00:51:19 -0500

    Fix special character in not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 function NotFound() {
   return (
     <div>
-      <h2>This page doesn't exist, Jedi knight!</h2>
+      <h2>{"This page doesn't exist, Jedi knight!"}</h2>
       <h3>Please, visit our home</h3>
       <Link href="/"><h2>Home</h2></Link>
     </div>
